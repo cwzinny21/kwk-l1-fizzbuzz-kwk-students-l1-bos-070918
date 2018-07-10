ceeaa6c0e97247d0a7ce28f18485102b9b080ffe
Cece Zinny <cwzinny@gmail.com>
2018-07-10 20:12:16 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,15 @@
+def fizzbuzz(int)
+  if int % 3 == 0 
+    "Fizz"
+    elsif int % 5 == 0 
+    "Buzz"
+  elsif int % 15 == 0 
+  "Fizzbuzz"
+    
+end
+end 
+
+puts fizzbuzz (5)
+puts fizzbuzz(12)
+puts fizzbuzz(10)
+puts fizzbuzz(15)
