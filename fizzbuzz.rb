9d2b39ba4e9b1eb5c211ceadd6107e62bbcb2037
Cece Zinny <cwzinny@gmail.com>
2018-07-10 20:28:36 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,8 @@
 def fizzbuzz(int)
   if int % 15 == 0
     #  you have to put 15 at the top because the computer reads top to bottom, which is why numbers like 15 and 60 were getting Fizz (because they were divisible by 3 so Fizz was put first)
+    # another way to do this is:
+    # if int % 3 == 0 && int % 5 == 0 --> FizzBuzz 
     "FizzBuzz"
    
     elsif int % 5 == 0 
